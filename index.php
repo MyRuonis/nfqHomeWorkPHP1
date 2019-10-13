c07469d54d64cc6ba04855f39cda088c6deabc2d
MyRuonis <kciulcinskas@gmail.com>
2019-10-13 18:22:47 +0300

    new

--- index.php
+++ index.php
@@ -13,11 +13,10 @@ use Nfq\Akademija\Soft as soft;
 use Nfq\Akademija\Strict as strict;
 
 try{
-    echo root\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
-    echo notTyped\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
-    echo soft\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
-    echo strict\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
-    asfafas;
+    echo "root: " . root\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
+    echo "Nfq\Akademija\NotType: " . notTyped\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
+    echo "Nfq\Akademija\Soft: " . soft\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
+    echo "Nfq\Akademija\Strict: " . strict\calculateHomeWorkSum(3, 2.2, '1') . "<br>";
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
