6da324aaf3ad4501bd921153c184a220f26a124a
MyRuonis <kciulcinskas@gmail.com>
2019-10-13 17:16:16 +0300

    test

---
+++ funkcijos/file1.php
@@ -0,0 +1,13 @@
+<?php
+    namespace root;
+
+    function calculateHomeWorkSum(...$numbers)
+    {
+        $sum = 0;
+        foreach($numbers as $number)
+        {
+            $sum += $number;
+        }
+        return $sum;
+    }
+?>
